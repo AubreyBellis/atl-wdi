bd68ae01664651b634ffe6189fcd6e4f634201b5
AubreyBellis <contactaubreybellis@gmail.com>
2017-07-07 06:40:47 -0400

    progress-bar homework

--- homework/unit_01/javaScript/progress-bar/hw.js
+++ homework/unit_01/javaScript/progress-bar/hw.js
@@ -4,15 +4,40 @@
 
 var timerUI = {
   drawNumericDisplay: function(timerValue){
-    // Your Code Here
+  
+    if (timerValue <= 10) {
+     numericDisplay.style.color = 'red';
+     var newSize = (1.5 * Math.pow(1.05, 11 - timerValue)).toFixed(2);
+     numericDisplay.style.fontSize = newSize + 'em';
+    }
   },
+
   drawProgressBars: function(timerValue){
-    // Your Code Here
+    
+    var progress = 100 - timerValue;
+    var bars = document.getElementsByClassName('progress-bar');
+    for (var i = 0; i < bars.length; i++) {
+    bars[i].style.width = progress + '%';
+    }
   },
   drawLitFuses: function(timerValue){
-    // Your Code Here
+   
+  var percentUnburnt = timerValue/100;
+  var numFuses = document.getElementsByClassName('fuse').length;
+  for (var i = 0; i < numFuses; i++){
+  document.getElementsByClassName('unburnt')[i].style.width = percentUnburnt*98 + '%';
+  document.getElementsByClassName('burnt')[i].style.width = (1 - percentUnburnt)*98 + '%';
+    }
   },
   drawCrawlers: function(timerValue){
-    // Your Code Here
+    
+    var timePassed = 100 - timerValue;
+    if (timerValue%2 === 0) {
+      document.getElementsByClassName('crawler')[0].style.marginTop = '0px';
+    }
+    else {
+      document.getElementsByClassName('crawler')[0].style.marginTop = '10px';
+    }
+    document.getElementsByClassName('crawler')[0].style.marginLeft = (timePassed*10) + 'px';
   }
 };
--- labs/unit_01/javaScript/js-functions/functions-lab-starter.js
+++ labs/unit_01/javaScript/js-functions/functions-lab-starter.js
@@ -1,27 +1,61 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(x,y) {
+if (x > y) {
+    return x;
+} else if (x < y) {
+    return y;
+ }
 }
+maxOfTwoNumbers(4,5);
 
 // Question 2
-function maxOfThree() {
 
+function maxOfThree(x,y,z) {
+    if (x > y && z) {
+        return x;
+    }else if (y > x && z) {
+        return y;
+    }else if (z > x && y) {
+        return z;
+    }
 }
+maxOfThree (5,7,6);
+
 
 // Question 3
-function isCharacterAVowel() {
 
+
+function isCharacterAVowel(x) {
+if(x === 'a' || x === 'e' || x === 'i' || x === 'o' || x === 'u') {
+    return true;
+}else {
+    return false;
+  }
+ }
+}
+isCharacterAVowel(v);
+
+// or
+
+function isCharacterAVowel(c) {
+  var vowels = ['a','e','i','o','u'];
+  if (vowels.indexOf(c) !== -1) {
+    return true;
+  } 
+  return false;
 }
 
 // Question 4
-function sumArray() {
+sum = [3,5,7,9];
+multiply = [1,3,5,7];
+
+function sumArray(sum) {
  
 }
 
 
 // Question 4
-function multiplyArray() {
-
+function multiplyArray(multiply) {
 }
 
 
