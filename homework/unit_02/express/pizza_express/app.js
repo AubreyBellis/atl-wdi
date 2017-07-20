95c84c1b19ad7f94afc25287f419b461f266378d
AubreyBellis <contactaubreybellis@gmail.com>
2017-07-19 22:31:38 -0400

    working on order

--- homework/unit_02/express/pizza_express/app.js
+++ homework/unit_02/express/pizza_express/app.js
@@ -3,10 +3,18 @@ const express = require('express');
 //save an express module as 'app'
 const app = express();
 // assigning 3000 as our port
+const hbs = require('hbs');
 
+app.set("view engine", "hbs");
 
+app.get('/', (req,res) => {
+     res.send(index);
+ });
 
+app.get('/topping/:type', function(req, res, next) {
 
+    res.send ;{req.params.type};
+});
 
 
 
@@ -20,6 +28,6 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen (3000, () => {
     console.log("server started on port " + port);
 });
