279ba253e2bef8f7f030ba40d2ec31e34a444518
AubreyBellis <contactaubreybellis@gmail.com>
2017-07-21 00:06:45 -0400

    new pirate challenge

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,37 @@
 //your code below
+//requirements
+const express = require("express");
+const router = express.Router();
+const pirates = require('../models/pirates.js');
+
+//all routes for /pirate
+router.get('/', (req,res)=> {
+    res.render('pirates/index', {
+        pirates:req.body
+    });
+});
+
+
+router.get('/new', (req,res)=> {
+    res.render('pirates/new');
+});
+
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const pirate = data.seededPirates[id]
+
+    res.render('pirates/show', {
+        pirate: pirate
+    });
+});
+
+router.post('/', (req,res)=> {
+    const newPirate = {
+    };
+    data.seededPirates.push(newPirate);
+});
+
+
+//exports
+module.exports = router;
--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,33 @@
-//your code here
+// your code here
+/* packages */
+var path        = require('path');
+var logger      = require('morgan');
+var express     = require('express');
+var hbs         = require('hbs');
+const bodyParser = require('body-parser');
+
+/* app settings*/
+var app         = express();
+var port        = process.env.PORT || 3000;
+
+var pirateController = require('./controllers/pirates.js');
+
+ app.use(bodyParser.urlencoded({ extended: false }));
+ app.use(bodyParser.json());
+
+
+
+
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', function(req,res) {
+  res.send('This is our Home Page');
+});
+app.use('/pirates', pirateController);
+
+// Start server
+app.listen(port, function() {
+  console.info('Server Ready.. ', port,"//", new Date());
+});
