809b5ded462212bdd043d59788ca07cdcfe607e2
AubreyBellis <contactaubreybellis@gmail.com>
2017-08-18 09:20:31 -0400

    finished homework

---
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -0,0 +1,20 @@
+1.) SELECT * FROM COUNTRY LIMIT 1;
+
+2.)SELECT language FROM countrylanguage WHERE countrycode = 'VAT';
+
+3.)
+SELECT * FROM countrylanguage WHERE language = 'Italian' ORDER BY percentage DESC;
+
+
+SELECT name FROM country WHERE code = 'SMR';
+4.)
+SELECT name FROM city WHERE countrycode = 'SMR';
+SELECT * FROM city WHERE name LIKE 'Serr%';
+
+5.)SELECT name FROM country WHERE code = 'BRA';
+
+6.) SELECT name FROM city WHERE id = '211';
+
+7.) SELECT name, district, countrycode FROM city WHERE population = 91084;
+
+------- Santa Monica, California USA--------- ANSWER
