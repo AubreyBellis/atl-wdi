0f94bcc51909ef25c88edfc4c15236ff7f286ac1
AubreyBellis <contactaubreybellis@gmail.com>
2017-08-22 09:39:45 -0400

    homework

---
+++ homework/ruby/ninja_skills.rb
@@ -0,0 +1,77 @@
+# Challenge 1
+a = true
+b = false
+puts a == b
+puts a != b
+
+# Challenge 2
+name = "nil"
+puts name
+puts "#{name} was here"
+print name
+puts name.class
+
+# Challenge 3
+d = 10
+puts d
+e = 5.5
+puts e
+d = e
+puts d
+
+# Challenge 4
+christine = "Hi, I'm Christine"
+likesTo = "long walks on the beach, kinda."
+tims = "#{christine} and I like #{likesTo}"
+puts tims
+g = "2"
+gToNumber = 2
+puts g + gToNumber
+puts g.to_i + gToNumber
+
+# Challenge 5
+puts "Multiply what number?"
+multiply = gets.chomp
+puts multiply.to_i * 2
+puts "Whats your name?"
+name = gets.chomp
+puts "What do you like to do?"
+fun = gets.chomp
+puts "#{name} really likes to #{fun}!!!"
+
+# Challenge 6
+myFirstArray = []
+myFirstArray = ["one", "two", "three", "four", "five", 1, 2, 3, 4]
+puts myFirstArray.last
+puts myFirstArray.first
+myFirstArray[1] = "Joe"
+myFirstArray[2] = 3
+myFirstArray.push("true")
+puts myFirstArray[3].class
+
+# Challenge 7
+myFavoriteAnimals = {
+    "Edgar"=> "Donkey",
+    "Apollo"=> "Axolotl",
+    "Coco"=> "Gecko",
+    "Muffin"=> "Bunny",
+    "Tucker"=> "Dog"
+}
+myFavoriteAnimals["Edgar"] = "Bear"
+myFavoriteMovie = {}
+myFavoriteMovie[:movie] = "Braveheart"
+
+# Challenge 8
+firstRange = (1..10)
+firstRange.to_a
+secondRange = (1...1000)
+secondRange.to_a
+for number in firstRange
+    puts number.to_s
+end
+x = 0
+secondRange = 
+
+# Challenge 9
+
+# Final Challenge
